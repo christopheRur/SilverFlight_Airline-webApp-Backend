bbca6f79cbda1727abee4e9434509ce0a3e5f821
Rurangwa <chrisruu@yahoo.com>
2023-08-25 20:55:47 +0800

    add test dependency

--- src/test/java/com/codelab/SilverAirline/SilverAirlineApplicationTests.java
+++ src/test/java/com/codelab/SilverAirline/SilverAirlineApplicationTests.java
@@ -1,8 +1,10 @@
 package com.codelab.SilverAirline;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 class SilverAirlineApplicationTests {
 
